83a1e3ddeebb0183daff7769d9af225c7ada9184
Christian Schwörer <christian.schwoerer@novatec-gmbh.de>
2021-04-16 14:46:55 +0200

    Update Spring Fu code format

--- spring-fu/src/main/kotlin/info/novatec/hello/world/Application.kt
+++ spring-fu/src/main/kotlin/info/novatec/hello/world/Application.kt
@@ -2,17 +2,16 @@ import org.springframework.fu.kofu.webApplication
 import org.springframework.fu.kofu.webmvc.webMvc
 
 val app = webApplication {
-
-	webMvc {
-		port = 8080
-		router {
-			GET("/hello") {
-				ok().body("Hello World!")
-			}
-		}
+    webMvc {
+        port = 8080
+        router {
+            GET("/hello") {
+                ok().body("Hello World!")
+	    }
 	}
+    }
 }
 
 fun main() {
-	app.run()
+    app.run()
 }
